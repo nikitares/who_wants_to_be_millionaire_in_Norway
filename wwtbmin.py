1331a0877b8582b9a9bd1a8a7ffe859f6fe99b99
nikitares01@gmail.com <nikitares01@gmail.com>
2023-02-21 11:12:34 +0100

    questions are working fine, 50/50 also working, but it program has some problems

--- wwtbmin.py
+++ wwtbmin.py
@@ -1,5 +1,94 @@
 import random
+
+#function for checking user input 
+def user_answer_check(user_answ):
+                if user_answ not in ["A", "B", "C", "D", "HINT"]:
+                    print("ERORR! Enter a valid letter from A to D\n")
+                    return True
 #create differnt class hints 
+class Hint:
+    def __init__(self, correct_answer, wrong_answers):
+        self.correct_answer = correct_answer
+        self.wrong_answers = wrong_answers  
+        self.used_audience_help = False
+        self.used_phone_friend = False
+        self.used_fifty_fifty = False
+        self.used_switch_question = False
+
+    def ask_hint(self):
+        user_hint = input("1) 50/50 2) Ask the audience 3) Phone a friend 4) Switch the question\n Enter a number of hint: ")
+        print("")
+        if user_hint == "1":
+            if self.used_fifty_fifty == True:
+                print("Sorry, you've already used the 50/50 help.")
+                quit()
+
+            
+            self.used_fifty_fifty = True
+
+            while True:
+                choice = self.fifty_fifty()
+                for i, answer in enumerate(choice):
+                    letter = chr(ord('A') + i) #what is this doing 
+                    print(f"{letter}. {answer}")
+                user_answer = input("Enter the letter of the correct answer: ").upper()
+                print("")
+
+            #function which checks user answer                      
+                if user_answer_check(user_answer) == True:
+                    continue
+                
+                
+                correct_answer_index = ord(user_answer) - ord('A')
+                if choice[correct_answer_index] == self.correct_answer: 
+                    return 
+                else:
+                    quit()
+
+                
+        elif user_hint == "2":
+        # code to implement "Ask the audience" hint
+            pass
+        elif user_hint == "3":
+        # code to implement "Phone a friend" hint
+            pass
+        elif user_hint == "4":
+        # code to implement "Switch the question" hint
+            pass
+        else:
+            print("Invalid hint choice. Please enter a number between 1 and 4.")  # handle invalid input
+            
+
+    def fifty_fifty(self):
+            choices = [self.correct_answer]
+            for answer in random.sample(self.wrong_answers, 1):
+                choices.append(answer)
+            random.shuffle(choices)
+            return choices
+
+
+#    def ask_the_audience(self, correct_answer):
+#        if not self.used_audience_help:
+#            # implementation of the ask_the_audience method
+#            self.used_audience_help = True
+#        else:
+#            print("Sorry, you've already used the ask the audience help.")
+
+#    def phone_a_friend(self, correct_answer):
+#        if not self.used_phone_friend:
+#            # implementation of the phone_a_friend method
+#            self.used_phone_friend = True
+#        else:
+#            print("Sorry, you've already used the phone a friend help.")
+
+
+
+#    def switch_the_question(self):
+#        if not self.used_switch_question:
+#            # implementation of the switch_the_question method
+#            self.used_switch_question = True
+#        else:
+#            print("Sorry, you've already used the switch the question help.")
 
 class Question:
     def __init__(self, prompt, correct_answer, wrong_answers, reward):
@@ -8,66 +97,39 @@ class Question:
         self.answers = [correct_answer] + wrong_answers
         random.shuffle(self.answers)
         self.reward = reward
-    
-    def fifty_fifty(self):
-        choices = [self.correct_answer]
-        random.shuffle(self.answers)
-        for answer in self.answers:
-            if answer != self.correct_answer:
-                choices.append(answer)
-            if len(choices) == 2:
-                break
-        return choices
+        #Here I use class Hint 
+        self.hint = Hint(correct_answer, wrong_answers)
     
     #create def answer_check for user_answer 
 
-    def ask(self, fifty_fifty_used = False):
+    def ask(self):
         while True:
 
-            if fifty_fifty_used == False:
-                fifty_fifty_message = "\nYou can use 50/50 help(input \"50\")"
-            elif fifty_fifty_used == True:
-                fifty_fifty_message = ""
-            
-            print(self.prompt + fifty_fifty_message)
+            print(self.prompt)
             for i, answer in enumerate(self.answers):
                 letter = chr(ord('A') + i) #what is this doing 
                 print(f"{letter}. {answer}")
-            user_answer = input("Enter the letter of the correct answer: ").upper()
+            user_answer = input("Enter the letter of the correct answer or HINT: ").upper()
             print("")
 
-            # ask the same question again, if TRUE
-            def user_answer_check():
-                if user_answer not in ["A", "B", "C", "D", "50"]:
-                    print("ERORR! Enter a valid letter from A to D\n")
-                    return True
-                       
-            if user_answer_check() == True:
+            #function which checks user answer                      
+            if user_answer_check(user_answer) == True:
                 continue
 
-            if user_answer == "50" and fifty_fifty_used == False:
-                for i, answer in enumerate(self.fifty_fifty()):
-                    letter = chr(ord('A') + i)
-                    print(f"{letter}. {answer}")
-                user_answer = input("Enter the letter of the correct answer: ").upper()
-                print("")
-                if user_answer_check() == True:
-                    continue
-                fifty_fifty_used = True
-            
-            elif user_answer == "50" and fifty_fifty_used == True:
-                print("You used your 50/50 hint\n")
-                continue
-            
-            #here mistake and thats why 50/50 isnt working 
-            correct_answer_index = ord(user_answer) - ord('A')
-            if self.answers[correct_answer_index] == self.correct_answer:
+            if user_answer == "HINT":
+                self.hint.ask_hint()
                 print(f"Correct! You have {self.reward} kroner!\n")
                 return self.reward
+            
             else:
-                print("Incorrect!")
-                print("You lost :(")
-                quit()
+                correct_answer_index = ord(user_answer) - ord('A')
+                if self.answers[correct_answer_index] == self.correct_answer:
+                    print(f"Correct! You have {self.reward} kroner!\n")
+                    return self.reward
+                else:
+                    print("Incorrect!")
+                    print("You lost :(")
+                    quit()
 
 #all questions it takes it from here(maybe letter i want to read this qustions from different file)
 questions = [
